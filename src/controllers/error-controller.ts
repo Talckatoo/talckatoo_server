f619c81cd90d100e71b05d9b8b106a9ac21fb750
mdmontes <manuel.montes0@gmail.com>
2023-05-26 10:55:47 -0400

    additional adjustments to error handling code

--- src/controllers/error-controller.ts
+++ src/controllers/error-controller.ts
@@ -1,14 +1,13 @@
 const AppError = require("../../utils/custom-error");
+
 import { Request, Response, NextFunction } from "express";
+
 const handleCastErrorDb = (err: any) => {
   const message = `invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 const handleDuplicateKeyDb = (err: any) => {
-  console.log(`This error handler called handleDuplicateKey is being used`)
-  console.log(`the error is`, err);
   const message = `dupliacte field value "${err.keyValue.userName}" please use another value`;
-  console.log(`${message}`);
   return new AppError(message, 400);
 };
 const handleValidationErrorDb = (err: any) => {
@@ -22,8 +21,7 @@ const handleJwtExpired = () =>
   new AppError("your token is expired please login again", 401);
 
 const sendErrorProd = (err: any, res: Response) => {
-  console.log("just testing right herre");
-  console.log(err);
+
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -45,7 +43,6 @@ const sendErrorDev = (err: any, res: Response) => {
     error: err,
   });
 
-  console.log(err.statusCode)
 };
 
 exports.globalErrorHandler = (
@@ -54,6 +51,7 @@ exports.globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "fail";
 
@@ -61,16 +59,16 @@ exports.globalErrorHandler = (
     sendErrorProd(err, res);
   } else if (process.env.NODE_ENV === "development") {
     let error = { ...err };
-    console.log(error);
-    console.log(error.code);
+
     if (err.name === "CastError") error = handleCastErrorDb(error);
     if (error.code === 11000) error = handleDuplicateKeyDb(error);
     if (err.name === "ValidationError") error = handleValidationErrorDb(error);
     if (error.name === "JsonWebTokenError") error = handleError();
     if (error.name === "TokenExpiredError") error = handleJwtExpired();
     
-    console.log('the error is', error);
-    console.log('the err is');
+    if (!error.message) {
+      error.message = err.message;
+    }
 
     sendErrorDev(error, res);
     
