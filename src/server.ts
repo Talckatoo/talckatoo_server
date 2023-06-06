0f5ab96be0ce36eaef292da41009c0de513bdef7
mdmontes <manuel.montes0@gmail.com>
2023-06-05 20:10:13 -0400

    implementing baseline code for socker.io

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app.ts");
+const socket = require("socket.io");
+import { Socket } from "socket.io";
+const { Types } = require("mongoose");
+
+
 dotenv.config({ path: "./config.env" });
 
 const DB = process?.env?.DATABASE?.replace(
@@ -14,9 +19,33 @@ const listener = async () => {
   await mongoose
     .connect(DB, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => console.log("database connection successful"));
   console.log(`Listening on Port ${PORT}!`);
 };
 const { PORT = 8000 } = process.env;
-app.listen(PORT, listener);
+const server = app.listen(PORT, listener);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+const onlineUsers = new Map<string, string>();
+
+io.on("connection", (socket: Socket) => {
+  const chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
