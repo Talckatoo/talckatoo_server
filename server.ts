0089013511cc3db0e56499678a6e7898cb8ab797
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2024-02-07 23:28:15 +0100

    remobing the comment

--- server.ts
+++ server.ts
@@ -35,7 +35,7 @@ const listener = async () => {
 };
 
 const { PORT = 8000 } = process.env;
-const server = app.listen(PORT || "8000", listener);
+const server = app.listen(PORT, listener);
 
 const io = socket(server, {
   cors: {
@@ -109,94 +109,94 @@ io.on("connection", (socket: Socket) => {
     }
   });
 
-  // socket.on("joinRandomChat", async (data: any) => {
-  //   try {
-  //     const { userName, profilePicture, language, id, email } = data;
+  socket.on("joinRandomChat", async (data: any) => {
+    try {
+      const { userName, profilePicture, language, id, email } = data;
 
-  //     // Log the socket ID
-  //     console.log(`Socket ID: ${socket.id}`);
+      // Log the socket ID
+      console.log(`Socket ID: ${socket.id}`);
 
-  //     // Check if there's an existing random conversation
-  //     const existingConversation = await RandomConversations.findOneAndUpdate(
-  //       { user2: null },
-  //       {
-  //         user2: {
-  //           userName,
-  //           profilePicture,
-  //           language,
-  //           id,
-  //           email,
-  //           socketId: socket.id,
-  //         },
-  //       },
-  //       { new: true }
-  //     );
+      // Check if there's an existing random conversation
+      const existingConversation = await RandomConversations.findOneAndUpdate(
+        { user2: null },
+        {
+          user2: {
+            userName,
+            profilePicture,
+            language,
+            id,
+            email,
+            socketId: socket.id,
+          },
+        },
+        { new: true }
+      );
 
-  //     if (!existingConversation) {
-  //       console.log("No existing random conversation");
+      if (!existingConversation) {
+        console.log("No existing random conversation");
 
-  //       // Create a new random conversation if none exists
-  //       const newConversation = await RandomConversations.create({
-  //         user1: {
-  //           userName,
-  //           profilePicture,
-  //           language,
-  //           socketId: socket.id,
-  //           id,
-  //           email,
-  //         },
-  //       });
+        // Create a new random conversation if none exists
+        const newConversation = await RandomConversations.create({
+          user1: {
+            userName,
+            profilePicture,
+            language,
+            socketId: socket.id,
+            id,
+            email,
+          },
+        });
 
-  //       console.log("New random conversation created:", newConversation);
+        console.log("New random conversation created:", newConversation);
 
-  //       // Emit event to the user who joined the random chat
-  //       io.to(socket.id).emit("randomResult", newConversation);
-  //     } else {
-  //       console.log(
-  //         "Existing random conversation found:",
-  //         existingConversation
-  //       );
+        // Emit event to the user who joined the random chat
+        io.to(socket.id).emit("randomResult", newConversation);
+      } else {
+        console.log(
+          "Existing random conversation found:",
+          existingConversation
+        );
 
-  //       // Emit event to the user who joined the random chat
-  //       io.to(socket.id).emit("randomResult", existingConversation);
+        // Emit event to the user who joined the random chat
+        io.to(socket.id).emit("randomResult", existingConversation);
 
-  //       io.to(existingConversation.user1.socketId).emit(
-  //         "randomResult",
-  //         existingConversation
-  //       );
+        io.to(existingConversation.user1.socketId).emit(
+          "randomResult",
+          existingConversation
+        );
 
-  //       // Delete the existing conversation
-  //       await RandomConversations.deleteOne({ _id: existingConversation._id });
-  //     }
-  //   } catch (error) {
-  //     console.error("Error joining random chat:", error);
-  //   }
-  // });
+        // Delete the existing conversation
+        await RandomConversations.deleteOne({ _id: existingConversation._id });
+      }
+    } catch (error) {
+      console.error("Error joining random chat:", error);
+    }
+  });
 
-  // socket.on("sendRandomMessage", async (data) => {
-  //   if (data.message) {
-  //     const options = {
-  //       method: "POST",
-  //       url: process.env.TRANSLATE_URL,
-  //       headers: {
-  //         "content-type": "application/json",
-  //         "X-RapidAPI-Key": process.env.TRANSLATE_API_KEY,
-  //         "X-RapidAPI-Host": process.env.API_HOST,
-  //       },
-  //       data: {
-  //         text: data.message,
-  //         target: data.language,
-  //       },
-  //     };
-  //     const response = await axios.request(options);
-  //     io.to(data.socketId).emit("getRandomMessage", {
-  //       ...data,
-  //       message: response.data[0].result.text,
-  //     });
-  //   } else {
-  //     io.to(data.socketId).emit("getRandomMessage", data);
-  //   }
-  // });
+  socket.on("sendRandomMessage", async (data) => {
+    if (data.message) {
+      const options = {
+        method: "POST",
+        url: process.env.TRANSLATE_URL,
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key": process.env.TRANSLATE_API_KEY,
+          "X-RapidAPI-Host": process.env.API_HOST,
+        },
+        data: {
+          text: data.message,
+          target: data.language,
+        },
+      };
+      const response = await axios.request(options);
+      io.to(data.socketId).emit("getRandomMessage", {
+        ...data,
+        message: response.data[0].result.text,
+      });
+    } else {
+      io.to(data.socketId).emit("getRandomMessage", data);
+    }
+  });
 
   socket.on("isTyping", (data: any) => {
     const sendUserSocket = onlineUsers.get(data.to);
@@ -276,103 +276,103 @@ io.on("connection", (socket: Socket) => {
     );
   });
 
-  // socket.on("updateProfile", (data: any) => {
-  //   const onlineFriends = data.onlineFriends;
-  //   for (let i = 0; i < onlineFriends.length; i++) {
-  //     let socketUserId = onlineUsers.get(onlineFriends[i]._id);
-  //     io.to(socketUserId).emit("getUpdateProfile", data);
-  //   }
-  // });
+  socket.on("updateProfile", (data: any) => {
+    const onlineFriends = data.onlineFriends;
+    for (let i = 0; i < onlineFriends.length; i++) {
+      let socketUserId = onlineUsers.get(onlineFriends[i]._id);
+      io.to(socketUserId).emit("getUpdateProfile", data);
+    }
+  });
 
-  // socket.on("sendFriendRequest", (data: any) => {
-  //   const sendUserSocket = onlineUsers.get(data.to);
-  //   if (sendUserSocket) {
-  //     io.to(sendUserSocket).emit("getFriendRequest", data);
-  //   }
-  // });
+  socket.on("sendFriendRequest", (data: any) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      io.to(sendUserSocket).emit("getFriendRequest", data);
+    }
+  });
 
-  // socket.on("acceptFriendRequest", (data: any) => {
-  //   const sendUserSocket = onlineUsers.get(data.to);
-  //   if (sendUserSocket) {
-  //     io.to(sendUserSocket).emit("getAcceptFriendRequest", data);
-  //   }
-  // });
+  socket.on("acceptFriendRequest", (data: any) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      io.to(sendUserSocket).emit("getAcceptFriendRequest", data);
+    }
+  });
 
-  // socket.on("disconnect", () => {
-  //   // Remove the disconnected socket from onlineUsers map
-  //   for (const [userId, socketId] of onlineUsers) {
-  //     if (socketId === socket.id) {
-  //       onlineUsers.delete(userId);
-  //       break;
-  //     }
-  //   }
-  //   io.emit("getUsers", Array.from(onlineUsers));
-  // });
+  socket.on("disconnect", () => {
+    // Remove the disconnected socket from onlineUsers map
+    for (const [userId, socketId] of onlineUsers) {
+      if (socketId === socket.id) {
+        onlineUsers.delete(userId);
+        break;
+      }
+    }
+    io.emit("getUsers", Array.from(onlineUsers));
+  });
 
-  // // setting up video call
+  // setting up video call
 
-  // // 1. Get logged in user ID: pass the userID in socket and get it from onlineUsers
+  // 1. Get logged in user ID: pass the userID in socket and get it from onlineUsers
 
-  // socket.on("callUser", (data: any) => {
-  //   const { userToCall, signalData, from, username, roomId } = data;
-  //   // getting a room from the socket adapter
-  //   // const { rooms } = io.sockets.adapter;
-  //   // const room = rooms.get(roomId);
+  socket.on("callUser", (data: any) => {
+    const { userToCall, signalData, from, username, roomId } = data;
+    // getting a room from the socket adapter
+    // const { rooms } = io.sockets.adapter;
+    // const room = rooms.get(roomId);
 
-  //   socket.join(roomId);
+    socket.join(roomId);
 
-  //   // Emit to the current socket
-  //   // socket.emit('roomCreated', { message: 'Room created!' });
+    // Emit to the current socket
+    // socket.emit('roomCreated', { message: 'Room created!' });
 
-  //   // Emit to all sockets in the room
-  //   io.to(roomId).emit("roomCreated", { message: "Room created!" });
+    // Emit to all sockets in the room
+    io.to(roomId).emit("roomCreated", { message: "Room created!" });
 
-  //   const sendUserSocket = onlineUsers.get(userToCall);
-  //   io.to(sendUserSocket).emit("callUser", {
-  //     signal: signalData,
-  //     from,
-  //     username,
-  //     roomId,
-  //     userToCall,
-  //   });
-  // });
+    const sendUserSocket = onlineUsers.get(userToCall);
+    io.to(sendUserSocket).emit("callUser", {
+      signal: signalData,
+      from,
+      username,
+      roomId,
+      userToCall,
+    });
+  });
 
-  // socket.on("answerCall", (data) => {
-  //   const { roomId } = data.callData;
-  //   socket.join(roomId);
-  //   io.to(roomId).emit("callAccepted", {
-  //     signal: data.signal,
-  //     call: data.callData,
-  //   });
+  socket.on("answerCall", (data) => {
+    const { roomId } = data.callData;
+    socket.join(roomId);
+    io.to(roomId).emit("callAccepted", {
+      signal: data.signal,
+      call: data.callData,
+    });
 
-  //   // socket.broadcast.to(roomId).emit("callAccepted", data.signal)
+    // socket.broadcast.to(roomId).emit("callAccepted", data.signal)
 
-  //   // io.to(sendUserSocket).emit("callAccepted", data.signal);
-  // });
+    // io.to(sendUserSocket).emit("callAccepted", data.signal);
+  });
 
-  // socket.on("leaveCall", (data) => {
-  //   const { userToCall, signalData, from, username, roomId } = data;
-  //   io.to(roomId).emit("leaveCall", { message: "Leave room!" });
-  // });
+  socket.on("leaveCall", (data) => {
+    const { userToCall, signalData, from, username, roomId } = data;
+    io.to(roomId).emit("leaveCall", { message: "Leave room!" });
+  });
 
-  // // 2. Create a room for video call
-  // socket.on("join", (roomId) => {
-  //   // getting a room from the socket adapter
-  //   const { rooms } = io.sockets.adapter;
-  //   const room = rooms.get(roomId);
-  //   // Scenario 1 when there's no one in the room
-  //   if (room === undefined) {
-  //     socket.join(roomId);
-  //     socket.emit("creates");
-  //   }
-  //   // Scenario 2 when 1 person is in the room
-  //   else if (room.size == 1) {
-  //     socket.join(roomId);
-  //     socket.emit("joined");
-  //   }
-  //   // Scenario 3 when there already 2 users in the room
-  //   else {
-  //     socket.emit("full");
-  //   }
-  // });
+  // 2. Create a room for video call
+  socket.on("join", (roomId) => {
+    // getting a room from the socket adapter
+    const { rooms } = io.sockets.adapter;
+    const room = rooms.get(roomId);
+    // Scenario 1 when there's no one in the room
+    if (room === undefined) {
+      socket.join(roomId);
+      socket.emit("creates");
+    }
+    // Scenario 2 when 1 person is in the room
+    else if (room.size == 1) {
+      socket.join(roomId);
+      socket.emit("joined");
+    }
+    // Scenario 3 when there already 2 users in the room
+    else {
+      socket.emit("full");
+    }
+  });
 });
